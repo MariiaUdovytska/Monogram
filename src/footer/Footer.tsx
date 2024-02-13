d9f256a3b47a7be202f5e5de381d0579283ba20e
Mariia <mariiaudovytska06@gmail.com>
2024-02-13 16:12:46 +0100

    initial commits for frontend

---
+++ src/footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../scss/footer/footer.scss";
+
+function Footer() {
+	return <div className="footer">footer test</div>;
+}
+
+export default Footer;
---
+++ src/menu/Menu.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../scss/menu/menu.scss";
+
+function Menu() {
+	return <div className="menu">menu test</div>;
+}
+
+export default Menu;
