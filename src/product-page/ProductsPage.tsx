f444ddf5c13e99ae466505f8a9e8baa9285919c8
Mariia <mariiaudovytska06@gmail.com>
2024-02-28 12:40:32 +0100

    included data products from json in carousel comp. DetailProducts

--- src/product-page/ProductsPage.tsx
+++ src/product-page/ProductsPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../scss/products-page/products-page.scss";
 import DetailProducts from "./detailProducts/DetailProducts";
+import productsData from "../data/products.json";
 
 function ProductsPage() {
+	const product = productsData[9];
 	return (
 		<div className="products-page h-100">
-			<DetailProducts />
+			<DetailProducts product={product} />
 		</div>
 	);
 }
--- src/product-page/detailProducts/DetailProducts.tsx
+++ src/product-page/detailProducts/DetailProducts.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "../../scss/products-page/products-detail.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, FreeMode, Thumbs } from "swiper/modules";
+import type { Swiper as SwiperClass } from "swiper";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
@@ -10,8 +11,28 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import DetailProductsInfo from "./DetailProductsInfo";
 
-function DetailProducts() {
-	const [thumbsSwiper, setThumbsSwiper] = useState(null);
+interface Product {
+	id: number;
+	title: string;
+	image: string;
+	image2: string;
+	images: string[];
+}
+
+interface ProductsProps {
+	product: Product;
+}
+
+function DetailProducts(data: ProductsProps) {
+	const [thumbsSwiper, setThumbsSwiper] = useState<SwiperClass | null>(null);
+
+	let productData = data.product;
+
+	let slidesMain = productData.images.map((image, index) => (
+		<SwiperSlide key={index}>
+			<img src={image} alt={productData.title} />
+		</SwiperSlide>
+	));
 
 	return (
 		<section className="product pb-4">
@@ -19,98 +40,45 @@ function DetailProducts() {
 				<div className="product__body">
 					<div className="product__body-main">
 						<h1 className="product__body-main-title text-uppercase">
-							Audio Console
+							{productData.title}
 							<span className="product__body-main-title-preorder ms-3 px-3">
 								Pre&nbsp;-&nbsp;order
 							</span>
 						</h1>
-						<div className="product__body-main-swiper">
-							<Swiper
-								style={{
-									"--swiper-navigation-color": "#fff",
-									"--swiper-pagination-color": "#fff",
-								}}
-								loop={true}
-								spaceBetween={10}
-								navigation={true}
-								thumbs={thumbsSwiper ? { swiper: thumbsSwiper } : undefined}
-								// thumbs={{ swiper: thumbsSwiper }}
-								modules={[FreeMode, Navigation, Thumbs]}
-								className="mySwiper2"
-							>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-10.jpg" />
-								</SwiperSlide>
-							</Swiper>
-							<Swiper
-								onSwiper={setThumbsSwiper}
-								loop={true}
-								spaceBetween={10}
-								slidesPerView={3}
-								freeMode={true}
-								watchSlidesProgress={true}
-								modules={[FreeMode, Navigation, Thumbs]}
-								className="mySwiper"
-							>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src="https://swiperjs.com/demos/images/nature-10.jpg" />
-								</SwiperSlide>
-							</Swiper>
-						</div>
+						{productData.images.length !== 0 ? (
+							<div className="product__body-main-swiper">
+								<Swiper
+									loop={true}
+									spaceBetween={10}
+									onSwiper={(swiper: SwiperClass) => setThumbsSwiper(swiper)}
+									modules={[FreeMode, Navigation, Thumbs]}
+									className="mySwiper2"
+								>
+									{slidesMain}
+								</Swiper>
+								<Swiper
+									loop={true}
+									spaceBetween={10}
+									slidesPerView={3}
+									freeMode={true}
+									watchSlidesProgress={true}
+									navigation={true}
+									modules={[FreeMode, Navigation, Thumbs]}
+									thumbs={thumbsSwiper ? { swiper: thumbsSwiper } : undefined}
+									className="mySwiper"
+								>
+									{slidesMain}
+								</Swiper>
+							</div>
+						) : (
+							<div className="product__body-main-img">
+								<img
+									src={productData.image}
+									alt={productData.title}
+									className="w-100"
+								/>
+							</div>
+						)}
 					</div>
 					<DetailProductsInfo />
 				</div>
