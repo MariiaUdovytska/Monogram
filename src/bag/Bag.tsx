55b01be2ce7e935e642233f60bbe26a2e86b9295
Mariia <mariiaudovytska06@gmail.com>
2024-03-07 13:35:00 +0100

    add styles for footer part Bag, add @mixin after-line for all place where need

--- src/bag/Bag.tsx
+++ src/bag/Bag.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../scss/bag/bag.scss";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import { ReactComponent as ArrowLeft } from "../image/logo/different/arrow-left.svg";
@@ -9,6 +9,8 @@ interface BagProps {
 }
 
 function Bag(props: BagProps) {
+	let [discount, setDiscount] = useState(false);
+
 	return (
 		<Offcanvas
 			show={props.show}
@@ -27,14 +29,47 @@ function Bag(props: BagProps) {
 				have chosen. Like, text, images, lists, etc.
 			</div>
 			<div className="bag__subtotal w-100 px-3 py-3 d-flex flex-column">
-				<div className="bag__subtotal-price w-100 d-flex flex-row justify-content-between">
+				<div className="bag__subtotal-price text-uppercase w-100 d-flex flex-row justify-content-between">
 					<span>subtotal</span>
 					<span>555</span>
 				</div>
-				<button type="button" className="w-100 py-2 my-4 rounded-pill">
-					Checkout
-				</button>
-				<span>Gift Card or Discount Code</span>
+				{discount ? (
+					<form className="d-flex flex-column align-items-center">
+						<input
+							className="text-uppercase w-100 py-3 my-4 rounded-pill"
+							type="text"
+							name="discount"
+							placeholder="Enter gift card or discount code"
+						/>
+						<p className="bag__subtotal-discount-p w-75 text-uppercase d-flex flex-row justify-content-between">
+							<button type="submit" className="ml-2 text-uppercase">
+								Apply Code
+							</button>
+							<button
+								type="button"
+								className="mr-2 text-uppercase"
+								onClick={() => setDiscount(false)}
+							>
+								Cancel
+							</button>
+						</p>
+					</form>
+				) : (
+					<form className="d-flex flex-column align-items-center">
+						<button
+							type="button"
+							className="bag__subtotal-btn text-uppercase w-100 py-3 my-4 rounded-pill"
+						>
+							Checkout
+						</button>
+						<button
+							onClick={() => setDiscount(true)}
+							className="bag__subtotal-discount text-uppercase d-flex flex-column justify-content-center"
+						>
+							Gift Card or Discount Code
+						</button>
+					</form>
+				)}
 			</div>
 		</Offcanvas>
 	);
